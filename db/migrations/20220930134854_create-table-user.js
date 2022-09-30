ca24e8700631dad6ce05ee740647fb5053a970da
BudiSetyo <budisetyo176@gmail.com>
2022-09-30 20:55:30 +0700

    feat: knex success

---
+++ db/migrations/20220930134854_create-table-user.js
@@ -0,0 +1,15 @@
+exports.up = async function (knex) {
+    await knex.raw('create extension if not exists "uuid-ossp"')
+    return knex.schema.createTable('users', (table) => {
+        table.uuid('id').defaultTo(knex.raw('uuid_generate_v4()')).primary()
+        table.string('email').notNullable().unique().comment('Email is unique')
+        table.string('password').notNullable()
+        table.string('name').notNullable()
+        table.timestamp('createdAt').defaultTo(knex.fn.now()).notNullable()
+        table.timestamp('updatedAt').defaultTo(knex.fn.now()).notNullable()
+    })
+}
+
+exports.down = function (knex) {
+    return knex.schema.dropTable('users')
+}
