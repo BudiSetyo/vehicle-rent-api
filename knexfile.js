c0ce4da754802d03dbfb288879343d1560d48cfe
BudiSetyo <budisetyo176@gmail.com>
2022-09-30 20:42:33 +0700

    feat: initial commit

---
+++ knexfile.js
@@ -0,0 +1,53 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+    development: {
+        client: 'pg',
+        connection: {
+            database: 'vehicle',
+            user: 'postgres',
+            password: 'postgres',
+        },
+        migrations: {
+            directory: './db/migrations',
+        },
+        seeds: {
+            directory: './db/seeds',
+        },
+    },
+
+    // staging: {
+    //   client: 'postgresql',
+    //   connection: {
+    //     database: 'my_db',
+    //     user:     'username',
+    //     password: 'password'
+    //   },
+    //   pool: {
+    //     min: 2,
+    //     max: 10
+    //   },
+    //   migrations: {
+    //     tableName: 'knex_migrations'
+    //   }
+    // },
+
+    // production: {
+    //   client: 'postgresql',
+    //   connection: {
+    //     database: 'my_db',
+    //     user:     'username',
+    //     password: 'password'
+    //   },
+    //   pool: {
+    //     min: 2,
+    //     max: 10
+    //   },
+    //   migrations: {
+    //     tableName: 'knex_migrations'
+    //   }
+    // }
+}
