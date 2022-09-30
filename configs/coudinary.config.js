dab06559cab7f2200823b194189fee3ba2936bda
BudiSetyo <budisetyo176@gmail.com>
2022-09-30 21:03:28 +0700

    feat: add cloudinary config

---
+++ configs/coudinary.config.js
@@ -0,0 +1,9 @@
+const cloudinary = require('cloudinary').v2
+
+cloudinary.config({
+    cloud_name: 'dprdhtoa3',
+    api_key: '288869758299212',
+    api_secret: 'uss__JQWImlVwyw64pshqqcZsy0',
+})
+
+module.exports = cloudinary
