0fa49976c0d70e6caaba85c152f5607f4d6757bb
BudiSetyo <budisetyo176@gmail.com>
2022-12-03 09:38:04 +0700

    feat: add payments validation

---
+++ src/validations/payments.js
@@ -0,0 +1,26 @@
+const Joi = require('joi')
+
+const addPayment = {
+    query: Joi.object.keys({
+        reservationId: Joi.string(),
+    }),
+    body: Joi.object.keys({
+        paymentType: Joi.string(),
+        statusPayment: Joi.boolean(),
+        paymentCode: Joi.string(),
+    }),
+}
+
+const editPayment = {
+    query: Joi.object.keys({
+        paymentId: Joi.string(),
+    }),
+    body: Joi.object.keys({
+        statusPayment: Joi.boolean(),
+    }),
+}
+
+module.exports = {
+    addPayment,
+    editPayment,
+}
