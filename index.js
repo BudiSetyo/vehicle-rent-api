3c472a12313eb73035792cbe27aea23d0207ad0f
BudiSetyo <budisetyo176@gmail.com>
2023-03-30 21:07:12 +0700

    feat: setup vercel config

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-require('dotenv').config();
+require('dotenv').config()
 const http = require('http')
 const App = require('./src/server')
 
-const server = http.createServer(App);
+const server = http.createServer(App)
 
 server.listen(process.env.PORT, () => {
-    console.log('Server Running at Port', process.env.PORT);
-  });
+    console.log('Server Running at Port', process.env.PORT)
+})
+
+module.exports = server
